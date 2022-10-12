c365ba9399d48cf23b911c73a77c6165925b7dd9
Tiana Kutin <tiana.kutin@gmail.com>
2022-10-12 17:13:31 +0200

    shranjevanje v datoteko

--- tekstovni_vmesnik.py
+++ tekstovni_vmesnik.py
@@ -4,10 +4,14 @@ from model import Knjiga, Stanje
 DODAJ_KNJIGO = 'dodaj'
 ZAKLJUCI = 'konec'
 
-#IME_DATOTEKE = 'stanje.json'
 
-#stanje = Stanje.preberi_iz_datoteke(IME_DATOTEKE)
-stanje = ['knjiga1']
+IME_DATOTEKE = "stanje.json"
+try:
+    stanje = Stanje.preberi_iz_datoteke(IME_DATOTEKE)
+except FileNotFoundError:
+    stanje = Stanje(knjige=[])
+
+
 
 def tekstovni_vmesnik():
     print(pozdravno_sporocilo())
@@ -18,18 +22,17 @@ def pozdravno_sporocilo():
     return "Živjo"
 
 def prikazi_osnovni_zaslon():
-    print(prikazi_aktualno_prebrano())
+    izpisi_stanje()
     while True:
         ukaz = preberi_ukaz()
         if ukaz == DODAJ_KNJIGO:
             dodaj_knjigo()
             prikazi_osnovni_zaslon()
         elif ukaz == ZAKLJUCI:
-            print("Nasvidenje!")
-            break
+            stanje.shrani_v_datoteko(IME_DATOTEKE)
+            print('Nasvidenje!')
+            break   
 
-def prikazi_aktualno_prebrano():
-    return stanje
 
 def preberi_ukaz():
     ukaz = input('Kaj želiš storiti? ')
@@ -42,9 +45,20 @@ def dodaj_knjigo():
     mnenje = input("Mnenje: ")
     datum = input("Datum, ko sem knjigo prebral: ")
     knjiga = Knjiga(naslov, avtor, ocena, mnenje, datum)
-    stanje = stanje.append(knjiga.naslov)
+    stanje.dodaj_knjigo(knjiga)
 
-
+def izpisi_stanje():
+    for knjiga in stanje.knjige:
+        print(f"""Naslov knjige: {knjiga.naslov}
+        Avtor: {knjiga.avtor}
+        Ocena: {knjiga.ocena}
+        Mnenje: {knjiga.mnenje}
+        Datum: {knjiga.datum}
+        """)
+    if not stanje.knjige:
+        print(
+            "Trenutno nimate še nobene knjige, zato morate eno najprej dodati."
+        )
 
 
 tekstovni_vmesnik()
